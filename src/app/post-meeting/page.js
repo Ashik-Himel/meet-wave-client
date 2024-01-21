822c8f18d88da2d1ab48d0f356cdc95a0f962e37
Tanvir Ramim <ramimhossan14@gmail.com>
2024-01-21 23:59:16 +0600

    post meeting route create

---
+++ src/app/post-meeting/page.js
@@ -0,0 +1,18 @@
+const PostMeeting = () => {
+    return (
+        <main>
+        <section className="mt-12 md:mt-6">
+          <div className="container">
+          <div>
+sfs
+            
+          </div>
+
+          </div>
+          
+        </section>
+      </main>
+    );
+};
+
+export default PostMeeting;
