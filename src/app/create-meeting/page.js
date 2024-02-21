891fd2d3c4311fbba4380b012d2f5135ad549dfc
mdridoyali <mdridoyislamr@gmail.com>
2024-02-21 15:25:32 +0600

    animation added to the create meeting page

--- src/app/create-meeting/page.js
+++ src/app/create-meeting/page.js
@@ -2,12 +2,21 @@
 import MeetingForm from "./MeetingForm";
 import useAllContext from "@/hooks/useAllContext";
 import { useRouter } from "next/navigation";
-import {FaArrowRight} from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
 import LoadingPage from "../loading";
+import { useEffect } from "react";
+
+
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 export default function Page() {
   const router = useRouter();
-  const {user, userLoaded} = useAllContext();
+  const { user, userLoaded } = useAllContext();
+
+  useEffect(() => {
+    AOS.init({})
+  }, [])
 
   if (!userLoaded) return <LoadingPage />;
   if (!user) return router.push('/login');
@@ -17,12 +26,14 @@ export default function Page() {
       <section className="my-12">
         <div className="container">
           <div className="flex flex-col md:flex-row gap-x-8 gap-y-6 justify-between md:items-center">
-            <div className="w-full max-w-[415px]">
+            <div data-aos="fade-right" data-aos-duration="2000" className="w-full max-w-[415px]">
               <h2 className="text-3xl font-semibold mb-4 !leading-[1.4]">Create meeting easily by completing the form!</h2>
               <p className="mb-6">Create a meeting by felling up this form. Then just share the meeting link to get started.</p>
               <label htmlFor="meeting-type" className="btn btn-primary [box-shadow:0px_0px_30px_rgba(33,128,232,0.25)]">Fill up now <FaArrowRight /></label>
             </div>
-            <MeetingForm />
+            <div data-aos="fade-left" data-aos-duration="2000" >
+              <MeetingForm />
+            </div>
           </div>
         </div>
       </section>
