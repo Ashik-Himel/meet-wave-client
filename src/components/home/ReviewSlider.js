030016407ad63711b7819129159146eb908ae500
mdridoyali <mdridoyislamr@gmail.com>
2024-02-21 11:49:01 +0600

    data setting done from the Backend

--- src/components/home/ReviewSlider.js
+++ src/components/home/ReviewSlider.js
@@ -1,10 +1,29 @@
-import React from 'react';
+"use client"
+import useAxiosPublic from '@/hooks/useAxiosPublic';
+import React, { useEffect, useState } from 'react';
 
 const ReviewSlider = () => {
-    
+    const [feedbacks, setFeedbacks] = useState([]);
+    const axiosPublic = useAxiosPublic();
+    console.log(feedbacks);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const res = await axiosPublic('/feedbacks');
+          setFeedbacks(res.data);
+        } catch (error) {
+          console.error('Error fetching feedbacks:', error);
+        }
+      };
+
+      fetchData();
+  
+    }, []);
+
     return (
         <div>
-            
+        
         </div>
     );
 };
