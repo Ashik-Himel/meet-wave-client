f328c0318af20e0c3b7471767ec90b257726aea2
mdridoyali <mdridoyislamr@gmail.com>
2024-02-21 11:14:10 +0600

    user email verification success

--- src/app/verify-email/page.js
+++ src/app/verify-email/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import useAllContext from '@/hooks/useAllContext';
 import { useRouter } from 'next/navigation';
 import { auth } from "@/configs/firebase.config";
@@ -7,54 +7,44 @@ import { sendEmailVerification } from 'firebase/auth';
 import toast from 'react-hot-toast';
 import LoadingPage from '../loading';
 
-const page = () => {
-    const router = useRouter();
-    const { user, userLoaded } = useAllContext();
-console.log(user?.emailVerified);
+const Page = () => {
+  const router = useRouter();
+  const { user, userLoaded } = useAllContext();
+  const [verificationEmailSent, setVerificationEmailSent] = useState(false);
 
-    const handleVerifyEmail = () => {
-        if (auth.currentUser) {
-             sendEmailVerification(auth.currentUser)
-             .then(()=>{
-                toast.success('Please check email')
-                setInterval(() => {
-                    window.location.reload(); // Reload the page every second
-                }, 5000);
-                setInterval(() => {
-                    window.location.reload(); // Reload the page every second
-                }, 10000);
-                setInterval(() => {
-                    window.location.reload(); // Reload the page every second
-                }, 15000);
-                setInterval(() => {
-                    window.location.reload(); // Reload the page every second
-                }, 20000);
-                setInterval(() => {
-                    window.location.reload(); // Reload the page every second
-                }, 25000);
-                setInterval(() => {
-                    window.location.reload(); // Reload the page every second
-                }, 30000);
-
-             })
-             .catch((error)=> toast.error(error.message))
-           
-        }
-
+  const handleVerifyEmail = () => {
+    if(!auth.currentUser)  toast.error('Please register before send verification email');
+    if (auth.currentUser) {
+      sendEmailVerification(auth.currentUser)
+        .then(() => {
+          toast.success('Please check email');
+          setVerificationEmailSent(true);
+          setInterval(() => {
+            window.location.reload();
+        }, 25000);
+        })
+        .catch((error) => toast.error(error.message));
     }
-    
+  };
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   if (!userLoaded) {
     return <LoadingPage />;
-  } else if (user.emailVerified) {
+  } else if (user?.emailVerified) {
     return router.push('/');
   }
 
-
-    return (
-        <div className='justify-center items-center flex '>
-            <button onClick={handleVerifyEmail} className='btn btn-primary text-lg'>Send verification email</button>
-        </div>
-    );
+  return (
+    <div className='gap-5 justify-center items-center flex '>
+      <button onClick={handleVerifyEmail} className='btn btn-primary text-lg'>Send verification email</button>
+      {verificationEmailSent && (
+        <button onClick={handleRefresh} className='btn btn-secondary border-neutral text-lg'>Refresh</button>
+      )}
+    </div>
+  );
 };
 
-export default page;
+export default Page;
